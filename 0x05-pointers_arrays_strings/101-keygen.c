3cc5018b61c8efa5b40fb47f52f8cf0dd890831b
CindyMacharia <cindymash29@gmail.com>
2021-09-07 00:09:33 +0300

    Random password generator

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,24 +1,76 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <time.h>
-
+  
+// Function to randomly generates password
+// of length N
+void randomPasswordGeneration(int N)
+{
+    // Initialize counter
+    int i = 0;
+  
+    int randomizer = 0;
+  
+    // Seed the random-number generator
+    // with current time so that the
+    // numbers will be different every time
+    srand((unsigned int)(time(NULL)));
+  
+    // Array of numbers
+    char numbers[] = "0123456789";
+  
+    // Array of small alphabets
+    char letter[] = "abcdefghijklmnoqprstuvwyzx";
+  
+    // Array of capital alphabets
+    char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+  
+    // Array of all the special symbols
+    char symbols[] = "!@#$^&*?";
+  
+    // Stores the random password
+    char password[N];
+  
+    // To select the randomizer
+    // inside the loop
+    randomizer = rand() % 4;
+  
+    // Iterate over the range [0, N]
+    for (i = 0; i < N; i++) {
+  
+        if (randomizer == 1) {
+            password[i] = numbers[rand() % 10];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else if (randomizer == 2) {
+            password[i] = symbols[rand() % 8];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else if (randomizer == 3) {
+            password[i] = LETTER[rand() % 26];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else {
+            password[i] = letter[rand() % 26];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+    }
+}
+  
+// Driver Code
 int main()
 {
-    srand((unsigned int)(time(NULL)));
-    int i;
-    char pass[12];
-
-    printf("Press enter to get a twelve-character password\n");
-    getchar();
-
-    for (i = 0; i < 4; i++) {
-        pass[i] = rand() % 9;
-        char capLetter = 'A' + (rand() % 26);
-        pass[i + 2] = capLetter;
-        char letter = 'a' + (rand() % 26);
-        pass[i + 3] = letter;
-        printf("%d%c%c", pass[i], pass[i + 2], pass[i + 3]);
-    }
-    printf("\n\n");
+    // Length of the password to
+    // be generated
+    int N = 10;
+  
+    // Function Call
+    randomPasswordGeneration(N);
+  
+    return 0;
 }
