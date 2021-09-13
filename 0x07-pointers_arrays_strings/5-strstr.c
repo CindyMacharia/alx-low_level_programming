f4c7abaf4a046af0323d4e15470e7d2098e4bd83
CindyMacharia <cindymash29@gmail.com>
2021-09-13 18:44:08 +0300

    Length of a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - checks the lenth of a substring
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "hello, world";
+    char *f = "world";
+    char *t;
+
+    t = _strstr(s, f);
+    printf("%s\n", t);
+    return (0);
+}
