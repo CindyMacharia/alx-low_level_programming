40b138f211ce6715171f5c5baf2fe473705f5a5d
CindyMacharia <cindymash29@gmail.com>
2021-09-28 02:52:24 +0300

    Defining a macro named SIZE

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#define SIZE 1024
+
+/**
+ * main - Create a header file that defines a macro named SIZE
+ *@ SIZE as an abbreviation for the token 1024.
+ *
+ * Return: Always 0.
+ */
+ int main(void)
+ {
+    int s;
+ 
+    s = 98 + SIZE;
+    printf("%d\n", s);
+    return (0);
+ }
