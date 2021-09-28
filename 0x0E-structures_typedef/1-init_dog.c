fddf14b470ac1482f65feadc02537bc8fcacaf2f
CindyMacharia <cindymash29@gmail.com>
2021-09-28 13:50:00 +0300

    Initializes the strut dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,6 +10,15 @@
  *
  * Description: General information abaout a dog
  */
+int main(void)
+{
+    struct dog my_dog;
+
+    my_dog.name = "Poppy";
+    my_dog.age = 3.5;
+    my_dog.owner = "Bob";
+    
+}
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == '\0')
@@ -17,4 +26,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);return (0)
 }
